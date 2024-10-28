c7e9e65319f7e8dcb4aba2585aace1ab0a5ba5e0
Iván Molinero <ivan.molinero@estudiants.urv.cat>
2024-10-29 00:08:06 +0100

    First MP3Player version
    Some methods set, as well as attributes.
    
    The actual problem lays on the configuration of the toolkit to reproduce audio

---
+++ src/testingMP3.java
@@ -0,0 +1,9 @@
+public class testingMP3 {
+    public static void main(String args[]) {
+        MP3Player reproducer = new MP3Player("TAKE-OFF-YOUR-DRESS-Kanye-West.mp3"); // Local file to test, correct URI specification.
+        reproducer.play();
+        do {
+            
+        } while(true); // Infinite loop.
+    }
+}
