912a6230be88f69d52770c990263e5d54095343c
ivanmoliinero <imolimort@gmail.com>
2024-11-04 07:18:57 -0800

    MP3 player with VLC implemented

--- src/code/MP3Player.java
+++ src/code/MP3Player.java
@@ -6,9 +6,25 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class MP3Player {
+    // ##################### INSTANCE ATTRIBUTES #####################
     private Player mp3Player;
     private FileInputStream file;
     private Thread reproducerThread;
+    private Object playPauseHandler; // We will have methods that reproduce and others that pause/resume. To avoid bad syncro, it is needed an object to do so.
+    /**
+     * To control the actual state of the player.
+     */
+    private boolean actuallyPlaying;
+    
+    /**
+     * To control whether the file has been correctly charged and the attributes as well.
+     */
+    private boolean fileCharged;
+    
+    /**
+     * To control whether the song has started or not, to restart the reproducer thread execution.
+     */
+    private boolean notStarted;
 
     /**
      * Constructor of the class. 
@@ -16,16 +32,27 @@ public class MP3Player {
      * @param fileName File of the first song to play.
      */
     public MP3Player(String fileName) {
+        // Initialisation of the thread and the synchronisation handler, both of which are always necessary.
+        reproducerThread = new Thread(() -> { 
+                playInternal();
+        });    
+        playPauseHandler = new Object();
         try {
             file = new FileInputStream(fileName);
             mp3Player = new Player(file);
+            actuallyPlaying = false; // Initially paused.
+            fileCharged = true;
+            notStarted = true;
         }
         catch (FileNotFoundException | JavaLayerException exc) {
-            System.err.println("File not found, instance not initialised");
+            System.err.println("##################### ERROR #####################\nFile not found, instance not initialised\n#################################################");
+            actuallyPlaying = false; // Initially paused.
+            fileCharged = false;
+            notStarted = true;
         }
     }
 
-    /* GETTERS */
+    // ##################### GETTERS #####################
 
     /**
      * Methods that returns the actual time of the active track.
@@ -33,38 +60,90 @@ public class MP3Player {
      * @return The time in seconds.
      */
     public double getActualTime() {
-        return(mp3Player.getPosition()*1000);
+        synchronized(playPauseHandler){
+            return(mp3Player.getPosition()/1000); // As the reproducer thread may be changing the state with playing, we must be syncronised.
+        }
     }
 
-    // TODO: Find an equivalent.
-    // /**
-    //  * Method that returns the total time of the active track.
-    //  * 
-    //  * @return The time in seconds.
-    //  */
-    // public double getTotalTime() {
-    //     return songURI.getDuration().toSeconds();
-    // }
-
+    // ##################### SETTERS #####################
     /**
      * Method to play the file in a thread (that will be stored in reproducerThread).
      */
     public void play() {
-        // A new thread has to be called in order to make other operations, such as pause the music.
-        reproducerThread = new Thread(() -> {
-            try {
-                mp3Player.play();
-            } catch (Exception e) {
-                System.err.println("Couldn't play the track.");
+        if(fileCharged) {
+            if(notStarted) {
+                notStarted = false;
+                actuallyPlaying = true; // Starting the playing.
+                reproducerThread.start(); // Starting the execution of the thread.
             }
-        }, "repro");
-        reproducerThread.start();
+            else if (!actuallyPlaying){
+                synchronized(playPauseHandler) {
+                    actuallyPlaying = true;
+                    playPauseHandler.notifyAll();
+                }
+            }
+        }
     }
 
     /**
      * Method to pause the track.
      */
     public void pause() {
-        // TODO: Implement this.
-    }   
+        synchronized (playPauseHandler) {
+            if(fileCharged && actuallyPlaying)
+                actuallyPlaying = false;
+        }
+    }
+
+    /**
+     * Method that allows the change to another MP3 file for its reproduction.
+     * 
+     * @param fileName The new path for the new MP3 file to load.
+     */
+    public void newMP3File(String fileName) {
+        synchronized(playPauseHandler) { // As the songs can be playing when the change occurs, it has to be a syncronised change.
+            try {
+                file = new FileInputStream(fileName);
+                mp3Player = new Player(file);
+                fileCharged = true;
+                if(!actuallyPlaying) play(); // If the file was paused, resume playing of the file.
+            }
+            catch (FileNotFoundException | JavaLayerException exc) {
+                System.err.println("##################### ERROR #####################\nFile not found, song not changed\n#################################################");
+                // File changed, not started and actually playing will remain as they were intially.
+            }
+        }
+    }
+
+    /**
+     * Method that is run by a separated thread to execute the playing of a song frame by frame.
+     */
+    private void playInternal() {
+        boolean songFinished = false;
+        while(!songFinished) {
+            try {
+                songFinished = !mp3Player.play(1); // Play a frame and evaluate its end.
+            }
+            catch (JavaLayerException exc) {
+                songFinished = true; // Error encountered, stop reproduction.
+            }
+            synchronized(playPauseHandler) {
+                if(!actuallyPlaying)         // If the song is paused ...
+                    try{
+                        playPauseHandler.wait(); // put this thread (reproducerThread) in waiting mode.
+                        // Reanudation occurs when resume method notifies all the threads, including this one, to continue their reproduction.
+                    }
+                    catch(InterruptedException exc) {
+                        break; // end of player.
+                    }
+            }
+        }
+        // SONG FINISHED.
+        synchronized(playPauseHandler) {
+            mp3Player.close(); // Close the song.
+            fileCharged = false;
+            notStarted = true;
+            actuallyPlaying = false;
+        }
+    }
 }
--- src/main/testMP3Player.java
+++ src/main/testMP3Player.java
@@ -1,14 +1,32 @@
 package main;
+
 import code.*;
 
 public class testMP3Player {
-    public static void main(String args[]) {
-        System.out.println("EXECUTED");
-        MP3Player rep = new MP3Player("/home/debian/DatosUNI/Xarxes De Dades/XD_spotify/data/ISABELLA.mp3");
+    public static void main(String args[]) throws InterruptedException {
+        System.out.println("Initialising incorrectly...");
+        MP3Player rep = new MP3Player("data/non_existent.mp3");
+        System.out.println("Setting a new MP3 file.");
+        rep.newMP3File("data/ISABELLA.mp3");       
+        System.out.println("Playing for 5 seconds...");
         rep.play();
-        System.out.println("HI"); // Proof to show that the execution continues.
-        // String stop = teclat.nextLine();
-        // System.out.println(stop.equals("s"));
+        Thread.sleep(5000);
+        System.out.println("Pausing for 5 seconds...");
+        rep.pause();
+        Thread.sleep(5000);
+        System.out.println("Resuming for 6 seconds...");
+        rep.play();
+        Thread.sleep(6000);
+        System.out.println("Changing song...");
+        rep.newMP3File("data/Family Business.mp3");      
+        System.out.println("Playing for 5 seconds...");
+        Thread.sleep(5000);
+        System.out.println("Changing to a non-existent song...");
+        rep.newMP3File("data/non_existent.mp3");
+        System.out.println("Pausing, then changing and then resuming...");
+        rep.pause();
+        rep.newMP3File("data/ISABELLA.mp3");
+        rep.play();
+        System.out.println("Playing the song indefinetely...");
     }
-    
 }
