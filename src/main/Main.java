101898f614f5b125464476c89de08611d9a662ab
javi <mejiasrjavi@gmail.com>
2024-11-05 20:44:20 +0100

    keyboard: Updated the Main.java example to show the behaviour of the function with timeout, and the other without

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,23 +1,43 @@
 public class Main {
     public static void main(String[] args) {
-        int timeout = 5; // Initial timeout in seconds
+        int timeout = 1000; // Initial timeout in ms
+        String acumStr = "";
 
         System.out.println("Press any key to reset the timeout. Timeout set to " + timeout + " seconds...");
 
+        // the first character is getted without timeout
+        int key = (int)KeyPressReader.getKey();
+
         while (true) {
-            int key = KeyPressReader.getKeyTimeout(1000);
-            if (key != -1) { // A key was pressed
-                System.out.println("You pressed: " + key);
-                if (key == 'q') { // Exit on 'q' key press
-                    break;
-                }
-                // Reset the timeout if any key is pressed
-                timeout = 5;
-            } else {
+
+            if (key == -1) {    // timeout
                 // Timeout occurred
                 System.out.println("Timeout occurred. Exiting...");
                 break;
+
+            } else if (key == KeyPressReader.BACKSPACE) {
+                int length = acumStr.length();
+                if (length > 0) {
+
+                    acumStr = acumStr.substring(0, length - 1);
+                }
+
+            } else if (key == 'q') {
+                break;
+
+            } else {
+                // other pressed keys
+                acumStr += (char)(key);
+                
             }
+
+            // ANSII escape code to clear the actual line of the command line 
+            // "\033[2K\r"
+            System.out.print("\033[2K\r" + acumStr);
+
+            key = KeyPressReader.getKeyTimeout(timeout);
+
+
         }
     }
 }
