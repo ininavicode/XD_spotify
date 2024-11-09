2643aa552154f6e09c655ff029c90eba3a9aa1c1
javi <mejiasrjavi@gmail.com>
2024-11-09 17:14:25 +0100

    main: Upgraded menu management structure

--- src/main/ClientMain.java
+++ src/main/ClientMain.java
@@ -12,36 +12,37 @@ import search_engine.*;
 import song.*;
 
 public class ClientMain {
+
     // ##################### data config #####################
     static final private String DATA_PATH = "datacli/";
 
     // ##################### visualization condig #####################
-    static final private int TEXT_INPUT_ROW = 10;
-    static final private int SONG_NAME_ROW = 3;
-    static final private int TIME_BAR_ROW = 10;
-    static final private int MAX_SONGS_SUGESTED = 8;
+    static final private int MAX_SONGS_SUGESTED = 9;
+    static final private int SONG_NAME_ROW = 11;
+    static final private int TEXT_INPUT_ROW = 13;
+    static final private int TIME_BAR_ROW = 15;
 
     // ##################### config constants #####################
     static final private int CONSOLE_WIDTH = 50;
     static final private int GET_KEY_TIMEOUT = 500; // ms
-    
+
     static final private int MENU_ERROR_STATE = -1;
     static final private int MENU_TEXT_INPUT = 0;
     static final private int MENU_REPRODUCING = 1;
     static final private int MENU_REQUEST_SELECTED_SONG = 2;
     static final private int MENU_SEARCH_ENGINE_REQUEST = 3;
-    
+
     // ##################### main variables #####################
     static private ConsoleMenu menu;
 
     static private String textInputString = "";
 
     static private int selectedSongIndex = -1;
-    static private int menuState = 0;
+    static private int menuState = 0, lastMenuState = -1;
 
     static private ArrayList<Song> songList;
     static private Client protocolClient;
-    
+
     static private VLCJPlayer vlcPlayer;
     static private Object UdpdateTimeThreadSync = new Object();
     static private volatile boolean reproducingState = false;
@@ -62,7 +63,6 @@ public class ClientMain {
      * Variable that indicates whether the song will be piled or played. 
      */
     private static boolean pileSong;
-    
 
     // Demo method to test the class
     public static void main(String[] args) throws SocketException, UnknownHostException, IllegalArgumentException {
@@ -81,7 +81,7 @@ public class ClientMain {
         protocolClient = new Client("127.0.0.1", 12000);
 
         vlcPlayer = new VLCJPlayer();
-        
+
         // ##################### main loop #####################
         int key;
         Thread updateTimeThread = new Thread(ClientMain::MENU_REPRODUCING_updateTimeThread);
@@ -89,9 +89,9 @@ public class ClientMain {
 
         // in case of error the application stays at the MENU_ERROR_STATE, an the captured message error will be printed
         String errorMsg = "";
-        
+
         // Display historial songs, in case they exist.
-        if(!songList.isEmpty()) {
+        if (!songList.isEmpty()) {
             selectedSongIndex = 0;
             menu.setMenuItems(songList);
             menu.setSelectedItem(0, 0, MAX_SONGS_SUGESTED);
@@ -101,10 +101,46 @@ public class ClientMain {
             switch (menuState) {
                 case MENU_TEXT_INPUT:
 
-                    displayTextInput();
-                    if(establishTimeout)
+                    // ############ initialization of section ############
+                    if (lastMenuState == MENU_REPRODUCING) {
+                        menu.clearScreen();
+
+                        if (!songList.isEmpty()) {
+                            menu.setSelectedItem(selectedSongIndex, 0, MAX_SONGS_SUGESTED);
+                        }
+                        else {
+                            selectedSongIndex = -1;
+                        }
+                        displayTextInput();
+
+                    }
+
+                    if (lastMenuState == MENU_SEARCH_ENGINE_REQUEST) {
+
+                        menu.setMenuItems(songList);
+
+                        if (!songList.isEmpty()) {
+                            
+                            if (selectedSongIndex >= songList.size()) {
+                                selectedSongIndex = songList.size() - 1;
+                            }
+                            else if (selectedSongIndex <= -1) {
+                                selectedSongIndex = 0;
+                            }
+
+                            menu.setSelectedItem(selectedSongIndex, 0, MAX_SONGS_SUGESTED);
+                        }
+                        
+                        displayTextInput();
+
+                    }
+
+                    lastMenuState = MENU_TEXT_INPUT;
+                    
+                    // ##################### section loop #####################
+                    if (establishTimeout) {
                         key = KeyPressReader.getKeyTimeout(GET_KEY_TIMEOUT);
-                    else {
+                    } else {
                         key = KeyPressReader.getKey();
                     }
 
@@ -113,28 +149,38 @@ public class ClientMain {
                         // ############ state change ############
                         menuState = MENU_SEARCH_ENGINE_REQUEST;
                         establishTimeout = false;
-                        menu.clearScreen();
                         break;
-                    }
-                    else if (key == KeyPressReader.INTRO || key == KeyPressReader.TAB) {
+                        
+                    } else if (key == KeyPressReader.INTRO || key == KeyPressReader.TAB) {
                         if (!songList.isEmpty() && (selectedSongIndex != -1)) {
                             // ############ state change ############
                             menuState = MENU_REQUEST_SELECTED_SONG;
                             establishTimeout = false;
-                            menu.clearScreen();
-    
                             break;
 
                         }
-                    }
-                    else {
+                    } else {
                         commonGetKeyTreatment(key);
                     }
-
 
                     break;
 
                 case MENU_REPRODUCING:
+
+                    if (lastMenuState != MENU_REPRODUCING) {
+                        synchronized (UdpdateTimeThreadSync) {
+                            reproducingState = true;
+                            UdpdateTimeThreadSync.notify();
+                        }
+    
+                        menu.clearScreen();
+    
+                        menu.printText(SONG_NAME_ROW, 0, songList.get(selectedSongIndex), ConsoleMenu.ALIGN_LEFT);
+                        displayReproducingState(reproducingState);
+
+                        lastMenuState = MENU_REPRODUCING;
+
+                    }
 
                     key = KeyPressReader.getKey();
 
@@ -146,39 +192,30 @@ public class ClientMain {
                                 UdpdateTimeThreadSync.notify();
                                 vlcPlayer.play(); // play the loaded file
                             }
-                        }
-                        else {
+                        } else {
                             synchronized (UdpdateTimeThreadSync) {
                                 reproducingState = false;
                                 vlcPlayer.pause();
-                                
+
                             }
                         }
 
                         // update the visualization state
                         displayReproducingState(reproducingState);
 
-                    }
-                    else if (key == KeyPressReader.ARROW_LEFT) {
+                    } else if (key == KeyPressReader.ARROW_LEFT) {
                         // mp3 rewind
                         vlcPlayer.goBack10Seconds();
-                        
-                    }
-                    else if (key == KeyPressReader.ARROW_RIGHT) {
+
+                    } else if (key == KeyPressReader.ARROW_RIGHT) {
                         // mp3 advance
                         vlcPlayer.advance10Seconds();
 
-                    }
-                    else if (key == 'q') {
+                    } else if (key == 'q') {
                         // ############ state change ############
                         menuState = MENU_TEXT_INPUT;
                         vlcPlayer.pause();
-                        menu.clearScreen();
-                        menu.setSelectedItem(selectedSongIndex, 0, MAX_SONGS_SUGESTED);
-                        if (!songList.isEmpty()) {
-                            menu.setSelectedItem(selectedSongIndex, 0, MAX_SONGS_SUGESTED);
-                        }
-                        establishTimeout = true;
+                        
                         synchronized (UdpdateTimeThreadSync) {
                             reproducingState = false;
                         }
@@ -188,11 +225,14 @@ public class ClientMain {
                     break;
 
                 case MENU_REQUEST_SELECTED_SONG:
-                    establishTimeout = false; // No timeout till first letter is inputted in next MENU_TEXT_INPUT.
+                    if (lastMenuState != MENU_REQUEST_SELECTED_SONG) {
+                        lastMenuState = MENU_REQUEST_SELECTED_SONG;
+                    }
+
                     // ############ server and mp3 operations ############
                     String mp3ToPlay = songList.get(selectedSongIndex).toFilename();
                     Song newSong = Song.filenameToSong(mp3ToPlay);
-                    if(!localHistorial.containsMP3(mp3ToPlay)) {
+                    if (!localHistorial.containsMP3(mp3ToPlay)) {
                         // Request to server if it does not exist in the local files.
                         try {
                             protocolClient.requestReceiveFile(songList.get(selectedSongIndex), DATA_PATH + mp3ToPlay);
@@ -209,25 +249,19 @@ public class ClientMain {
 
                     // ############ state change ############
                     menuState = MENU_REPRODUCING;
-                    synchronized (UdpdateTimeThreadSync) {
-                        reproducingState = true;
-                        UdpdateTimeThreadSync.notify();
-                    }
-
-                    menu.clearScreen();
-                    
-                    menu.printText(SONG_NAME_ROW, 0, songList.get(selectedSongIndex), ConsoleMenu.ALIGN_LEFT);
-                    displayReproducingState(reproducingState);
-
                     break;
 
                 case MENU_SEARCH_ENGINE_REQUEST:
+                    if (lastMenuState != MENU_SEARCH_ENGINE_REQUEST) {
+                        lastMenuState = MENU_SEARCH_ENGINE_REQUEST;
+                    }
+
                     // protocol process ...
                     Protocol.ResponseSearchEngine_t response;
                     try {
                         protocolClient.requestSearchEngine(textInputString, clientCookie);
                         response = protocolClient.receiveSearchEngine();
-                        
+
                     } catch (IOException e) {
                         // ############ state change ############
                         menuState = MENU_ERROR_STATE;
@@ -236,7 +270,6 @@ public class ClientMain {
                     }
 
                     // update songList
-
                     // TODO: the ArrayList<Song> has no clone method so all the usage of this structure is a reference copy
                     // This reference copy happens at the menu class to so there should be created a method to copy this structure
                     /*
@@ -247,46 +280,30 @@ public class ClientMain {
                      * has indeed no options with those search parameters.
                      */
                     // TODO: (optional) Indicate that an empty list has been returned instead of displaying void.
-                    if(response.cookie == -1)
+                    if (response.cookie == -1) {
                         songList = localHistorial.getSongsList();
-                    else
-                        songList = response.songList; 
+                    } else {
+                        songList = response.songList;
+                    }
 
                     // update the cookie
                     clientCookie = response.cookie;
 
-                    if (!songList.isEmpty()) {
-                        if (selectedSongIndex >= songList.size()) {
-                            selectedSongIndex = songList.size() - 1;
-                        }
-                        if (selectedSongIndex == -1) {
-                            selectedSongIndex = 0;
-                        }
-                        menu.setMenuItems(songList);
-                        menu.setSelectedItem(selectedSongIndex, 0, MAX_SONGS_SUGESTED);
-                    }
-                    else {
-                        selectedSongIndex = -1;
-                    }
-
                     // ############ state change ############
                     menuState = MENU_TEXT_INPUT;
-                    if (!songList.isEmpty()) {
-                        menu.setSelectedItem(selectedSongIndex, 0, MAX_SONGS_SUGESTED);
-                    }
-                    
+
                     break;
-            
+
                 case MENU_ERROR_STATE:
                     System.err.print("\nError: " + errorMsg);
-                    while (true) { 
-                        
+                    while (true) {
+
                     }
-                }
-            
+
+            }
         }
 
-    } 
+    }
 
     private static void MENU_REPRODUCING_updateTimeThread() {
         while (true) {
@@ -299,11 +316,11 @@ public class ClientMain {
                         return; // Exit the loop if the thread is interrupted
                     }
                 }
-    
+
                 // If not paused, update the time
                 menu.printLoadingBar(TIME_BAR_ROW, vlcPlayer.getActualTime(), vlcPlayer.getTotalLength());
             }
-    
+
             try {
                 Thread.sleep(900); // Adjust the time as needed
             } catch (InterruptedException e) {
@@ -318,33 +335,34 @@ public class ClientMain {
             establishTimeout = true;
             if (key == KeyPressReader.BACKSPACE) {
                 if (textInputString.length() > 0) {
-                    
+
                     textInputString = textInputString.substring(0, textInputString.length() - 1);
                 }
+            } else {
+                textInputString += (char) key;
             }
-            else {
-                textInputString += (char)key;
-            }
-            
-        }
-        else {
+
+            displayTextInput();
+
+        } else {
             // update select index
             establishTimeout = false;
             if (key == KeyPressReader.ARROW_UP) {
                 if (selectedSongIndex > 0) {
                     selectedSongIndex--;
                 }
-            }
-            else if  (key == KeyPressReader.ARROW_DOWN) {
+            } else if (key == KeyPressReader.ARROW_DOWN) {
                 if (selectedSongIndex < (MAX_SONGS_SUGESTED - 1)) {
                     selectedSongIndex++;
                 }
             }
             if (!songList.isEmpty()) {
                 menu.setSelectedItem(selectedSongIndex, 0, MAX_SONGS_SUGESTED);
+                displayTextInput();
+
             }
         }
-            
+
     }
 
     static private void displayTextInput() {
@@ -358,8 +376,7 @@ public class ClientMain {
         if (state == true) {
             menu.printText(TIME_BAR_ROW - 1, 7, ConsoleMenu.LEFT_ARROW + "     " + ConsoleMenu.PLAY + "     " + ConsoleMenu.RIGHT_ARROW, ConsoleMenu.ALIGN_CENTER);
 
-        }
-        else {
+        } else {
             menu.printText(TIME_BAR_ROW - 1, 7, ConsoleMenu.LEFT_ARROW + "     " + ConsoleMenu.PAUSE + "     " + ConsoleMenu.RIGHT_ARROW, ConsoleMenu.ALIGN_CENTER);
 
         }
